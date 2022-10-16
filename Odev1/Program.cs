0e492edf0cad3ee0e35a992782311c9bacf1484c
user <user@DESKTOP-M75MBEG>
2022-10-16 10:23:08 +0300

    Add project files.

---
+++ Odev1/Program.cs
@@ -0,0 +1,154 @@
+﻿namespace Odev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Öğrenci Vize Final Notu Hesaplama");
+
+            // Console'^dan girilen vize ve final notları ile ortalamayı hesaplayan bu ortalamaya göre harfnotunu bulup ekrana ortalama ve harfnotunu yazan prog. yazınız.. tanımladığım değişkenleri kullanınız... 
+
+
+            float vize, final, ort=0;
+            
+            // vize notu isteyiniz
+            Console.WriteLine("vize notu giriniz");
+            vize = Convert.ToInt32(Console.ReadLine());
+            
+            // final notu isteyiniz
+            Console.WriteLine("final notunu giriniz");
+            final = Convert.ToInt32(Console.ReadLine());
+            
+            // ortalama hesaplayınız
+            ort = (float)((vize * 0.4) + (final * 0.6));
+
+
+            // ortalam vize notunun %40 finalnotunun %60 formülü ile hesaplanır. Formül (vizenotu * 0.4) + (finalnotu * 0.6)
+            if (ort < 25)
+            {
+                string harfNotu = "FF";
+
+                // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+            }
+
+            if (25 <= ort && ort<50)
+            {
+                string harfNotu = "DC";
+
+                // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+            }
+
+            else if (50<=ort && ort<65)
+            {
+                string harfNotu = "CC";
+
+                // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+            }
+
+            else if (65<=ort && ort<75)
+            {
+                string harfNotu = "BB";
+
+                // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+            }
+
+            else if (75<=ort && ort < 85)
+            {
+                string harfNotu = "BA";
+
+                // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+            }
+            else
+            { 
+                string harfNotu = "AA";
+
+                // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+            }
+
+            switch (ort)
+            {
+                case >0 and < 25:
+                    
+                    string harfNotu = "FF";
+
+                    // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                    Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+                    break;
+            }
+
+            switch (ort)
+            {
+                case >= 25 and < 50:
+
+                    string harfNotu = "DC";
+
+                    // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                    Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+                    break;
+            }
+            switch (ort)
+            {
+                case >= 50 and < 65:
+
+                    string harfNotu = "CC";
+
+                    // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                    Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+                    break;
+            }
+
+            switch (ort)
+            {
+                case >= 65 and < 75:
+
+                    string harfNotu = "BB";
+
+                    // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                    Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+                    break;
+            }
+            switch (ort)
+            {
+                case >=75 and < 85:
+
+                    string harfNotu = "BA";
+
+                    // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                    Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+                    break;
+            }
+            switch (ort)
+            {
+                case >= 85 and <= 100:
+
+                    string harfNotu = "AA";
+
+                    // harfNotu =  Eğer ortalama 85 ile 100 arasında ise AA, 75 ile 84 arasında ise BA,65 ile 74 arasında ise BB,50 ile 64 arasında ise CC,25 ile 49 arasında ise DC, 25'ten küçük ise FF olarak bulunmalıdır..
+
+                    Console.WriteLine($"Ortalamanız ={ort} HarfNotunuz = {harfNotu}");
+                    break;
+            }
+
+            // todo: bütün  istenilenleri switch-case ve if karar mekanizmaları kullanarak yapınız
+            Console.WriteLine("Kapatmak için bir tuşa basınız");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Odev2/Program.cs
@@ -0,0 +1,192 @@
+﻿namespace Odev2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Console'dan alınan ay'ın mevsimini yazdıran prorgramı yapınız.
+            Console.WriteLine("Lütfen ay giriniz");
+            string ay = Console.ReadLine();
+            
+            string mevsim;
+            int aysirasi;
+            if (ay.ToUpper() == "OCAK")
+            {
+                mevsim = "KIŞ";
+                aysirasi = 1;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+
+            else if (ay.ToUpper() == "ŞUBAT")
+            {
+                mevsim = "KIŞ";
+                aysirasi = 2;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+            else if (ay.ToUpper() == "MART")
+            {
+                mevsim = "KIŞ";
+                aysirasi = 3;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+            else if (ay.ToUpper() == "NİSAN")
+            {
+                mevsim = "İLKBAHAR";
+                aysirasi = 4;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+
+            else if (ay.ToUpper()=="MAYIS")
+            {
+                mevsim = "İLKBAAR";
+                aysirasi = 5;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+            else if (ay.ToUpper() == "HAZİRAN")
+            {
+                mevsim = "İLKBAAR";
+                aysirasi = 6;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+            else if (ay.ToUpper() == "TEMMUZ")
+            {
+                mevsim = "YAZ";
+                aysirasi = 7;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+            else if (ay.ToUpper() == "AĞUSTOS")
+            {
+                mevsim = "YAZ";
+                aysirasi = 8;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+            else if (ay.ToUpper() == "EYLÜL")
+            {
+                mevsim = "YAZ";
+                aysirasi = 9;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+
+            else if (ay.ToUpper() == "EKİM")
+            {
+                mevsim = "SONBAHAR";
+                aysirasi = 10;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+            else if (ay.ToUpper() == "KASIM")
+            {
+                mevsim = "SONBAHAR";
+                aysirasi = 11;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+            else if (ay.ToUpper() =="ARALIK")
+            {
+                mevsim = "SONBAHAR";
+                aysirasi = 12;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+            }
+
+            switch (ay.ToUpper())
+            {
+                case("OCAK"):
+                    mevsim = "KIŞ";
+                    aysirasi=1;
+                Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+            
+            
+                case ("ŞUBAT"):
+                    mevsim = "KIŞ";
+                    aysirasi = 2;
+                    Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+           
+                case ("MART"):
+                    mevsim = "KIŞ";
+                    aysirasi = 3;
+                    Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+            
+                case ("NİSAN"):
+                    mevsim = "İLKBAHAR";
+                    aysirasi = 4;
+                    Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+           
+                case ("MAYIS"):
+                    mevsim = "İLKBAHAR";
+                    aysirasi = 5;
+                    Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+            
+                case ("HAZİRAN"):
+                    mevsim = "İLKBAHAR";
+                    aysirasi = 6;
+                    Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+            
+       
+                case ("TEMUZ"):
+                    mevsim = "YAZ";
+                    aysirasi = 7;
+                    Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+            
+            
+                case ("AĞUSTOS"):
+                    mevsim = "YAZ";
+                    aysirasi = 8;
+                    Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+            
+            
+            
+                case ("EYLÜL"):
+                    mevsim = "YAZ";
+                    aysirasi = 9;
+                    Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+                case ("EKİM"):
+                    mevsim = "SONBAHAR";
+                    aysirasi = 10;
+                    Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+
+                case ("KASIM"):
+                    mevsim = "SONBAHAR";
+                    aysirasi = 11;
+                    Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+
+                case ("ARALIK"):
+                    mevsim = "SONBAHAR";
+                    aysirasi = 12;
+                    Console.WriteLine($"{mevsim}\n{aysirasi}.Ay ");
+                    break;
+
+                    default:
+                    Console.WriteLine("geçerli bir ay ismi giriniz");
+                    break;
+
+            }
+            
+            // eğer ay;  aralık-ocak-şubat      girilirse ekrana KIŞ 
+            // mart-nisan-mayıs       girilirse ekrana İLK BAHAR
+            // haziran-temmuz-ağustos girilirse ekrana YAZ 
+            // eylük-ekim-kasım       girilirse ekrana SON BAHAR
+            // mesajlarını veren programı yazınız.. Girilen ay için kaçıncı ay olduunu yazınız ekrana n. Ay şeklinde yazınız..
+
+            // örnek;
+            // klavyeden girilen ocak ayı için çıktı aşağıdaki gibi olmalıdır;
+            // KIŞ
+            // 1. Ay
+
+
+            // todo: bütün  istenilenleri switch-case ve if karar mekanizmaları kullanarak yapınız
+
+            Console.WriteLine("Kapatmak için bir tuşa basınız");
+            Console.ReadKey();
+        }
+    }
+}
